545358131110eefec9ab99ed85eef4dfea758111
hjhjhj0917 <abc22842199@gmail.com>
2025-10-22 09:50:35 +0900

    vue 학습

---
+++ src/Ex01.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>{{ greeting }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {greeting: 'Hellow Vue!'}
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/CounterCompnent.vue
@@ -0,0 +1,28 @@
+<template>
+  <p>{{ count }}</p>
+  <button @click="increment">증가</button>
+</template>
+
+<script>
+import { ref, onMounted, onUnmounted } from "vue";
+export default {
+  setup() {
+    const count = ref(0);
+    const increment = () => {
+      count.value++;
+    };
+    onMounted( () => {
+      console.log('컴포넌트가 마운트됐어요!');
+    });
+    onUnmounted( () => {
+      console.log('컴포넌트가 제거됏됐어요!');
+    });
+    return {count, increment };
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
